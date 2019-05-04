8bced8a2c4889402cafaf62e591513c47273b167
lawrencemboya <lawrencemboya@gmail.com>
2019-05-04 14:34:48 -0400

    update

--- FurtuneTeller.java
+++ FurtuneTeller.java
@@ -1,9 +1,15 @@
-
-public class FurtuneTeller {
+import java.util.Scanner;
+public class FurtuneTellerExercise {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("What is your age?");
+		int age = input.nextInt();
+		System.out.println("You are " + age + " years old");
+		
+		input.close();
 	}
 
 }
