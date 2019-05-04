0e04cdf0a192af072e38dc97d397c37674f9b897
aprempe <aprempe@yahoo.com>
2019-05-04 15:02:52 -0400

    clean up files

--- src/Application.java
+++ src/Application.java
@@ -1,10 +1,35 @@
-
+import java.util.Scanner;
 public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
+
+
+
+
+
+		// TODO Auto-generated method stub
+
+
+
+
+	Scanner input = new Scanner(System.in);
+
+	
+
+	System.out.println("How many siblings do you have");
+
+	int mySiblings =  input.nextInt();
+
+	
+
+	System.out.println(mySiblings + "Siblings");  
 
 	}
 
 }
+
+	
+
+			
