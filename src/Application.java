3197025dec2316c9f57ea63d2fc7268498cfdb6d
anitaelfreada2001 <anitaelfreda2001@yahoo.com>
2019-05-04 14:49:23 -0400

    number of siblings

--- src/Application.java
+++ src/Application.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
 
 public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("How many siblings do you have");
+		
+		int mySiblings = input.nextInt();
+		
+		System.out.print(mySiblings);
 
-	}
+	} 
 
 }
