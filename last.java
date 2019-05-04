cc85b86615fdb88dfa43ce789e48f9c5aecc2e66
namilan <nichole.milan@gmail.com>
2019-05-04 14:35:43 -0400

    my part

--- last.java
+++ last.java
@@ -1,8 +1,17 @@
+import java.util.Scanner;
 
 public class last {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void last(String[] args) {
+	
+		Scanner input = new Scanner(System.in);
+		
+		//Ask for the users last name
+				System.out.println("Now, may I have your last name?");
+				String userLast = input.next();
+				
+		//Text response for users last name
+				System.out.println("Ah, " + userLast.toUpperCase() + ", strong last name. Suits you. ");
 
 	}
 
