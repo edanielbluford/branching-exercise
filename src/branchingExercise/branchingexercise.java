3882e9f6d1391566674ec6a80780c0947472023b
aprempe <aprempe@yahoo.com>
2019-05-04 14:26:12 -0400

    master

---
+++ src/branchingExercise/branchingexercise.java
@@ -0,0 +1,25 @@
+package branchingExercise;
+
+
+
+import java.util.Scanner;
+
+public class branchingexercise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+
+
+	Scanner input = new Scanner(System.in);
+	
+	System.out.println("How many siblings do you have");
+	int mySiblings =  input.nextInt();
+	
+	System.out.println(mySiblings + "Siblings");  
+	}
+}
+	
+			
+			
