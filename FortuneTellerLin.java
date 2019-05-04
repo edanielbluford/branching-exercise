b3559d31010744c8f67c40bd9be6169a0e4b6772
klin2006 <klin2006@gmail.com>
2019-05-04 15:39:31 -0400

    I have made changes.

--- FortuneTellerLin.java
+++ FortuneTellerLin.java
@@ -4,12 +4,14 @@ public class FortuneTeller {
 
 	public static void main(String[] args) {
 		
-
-	
 	Scanner input = new Scanner (System.in);
-	System.out.println("What is your first name?");
-	String firstName = input.nextLine();
 	
+
+	System.out.println("What is your first name?");
+		String firstName = input.nextLine();
+	
+
+	input.close();
 	
 	
 	}
